26f80c9b7e25cd597a564e4cf5e4792a930df1f1
Documenter.jl <documenter@juliadocs.github.io>
2023-01-10 21:31:02 +0000

    build based on ae069af

---
+++ docs/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API-Reference","page":"API Reference","title":"API Reference","text":"","category":"section"},{"location":"api/#SoundWaves.jl-Functions","page":"API Reference","title":"SoundWaves.jl Functions","text":"","category":"section"},{"location":"","page":"SoundWaves","title":"SoundWaves","text":"CurrentModule = SoundWaves\nDocTestSetup = quote\n    using SoundWaves\nend","category":"page"},{"location":"#SoundWaves.jl","page":"SoundWaves","title":"SoundWaves.jl","text":"","category":"section"}]
+}
